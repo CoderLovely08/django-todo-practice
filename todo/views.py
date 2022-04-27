dce64a73c2657cc742b470b4603f8c513b01e49c
Lovely Sharma <lovelysingeras297@gmail.com>
2022-04-27 21:43:22 +0530

    added links to other pages

--- todo/views.py
+++ todo/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'todo/home.html')
+
+def portfolio(request):
+    return render(request,'todo/portfolio.html')
+
+def contact(request):
+    return render(request,'todo/contact.html')
