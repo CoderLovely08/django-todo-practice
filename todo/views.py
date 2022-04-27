cc61c763fc26327346bcf39927893f5ab7bce3d1
Lovely Sharma <lovelysingeras297@gmail.com>
2022-04-27 23:27:30 +0530

    learned to save data to database

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
+import imp
 from django.shortcuts import render
+from .models import Student
 
 def index(request):
     return render(request,'todo/home.html')
@@ -7,4 +9,16 @@ def portfolio(request):
     return render(request,'todo/portfolio.html')
 
 def contact(request):
+    print(request.method)
+    if request.method=='POST':
+        print(request.POST.get('first_name'))
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        student_age=request.POST.get('student_age')
+
+        query = Student(first_name=first_name,last_name=last_name,student_age=student_age)
+        query.save()
+
+        return render(request,'todo/contact.html')
+    else: print('ahhh')
     return render(request,'todo/contact.html')
